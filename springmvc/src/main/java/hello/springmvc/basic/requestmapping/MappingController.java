23cc25959d9e7f23e031d0b530eed9dd74f486d8
heroku test <wjstngus1031@gmail.com>
2022-01-03 23:33:05 +0900

    매핑 테스트

--- springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -1,7 +1,15 @@
 package main.java.hello.springmvc.basic.requestmapping;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+@RestController
 public class MappingController {
+    private Logger log = LoggerFactory.getLogger(getClass());
 
-
+    @RequestMapping("/hello-basic")
+    public String helloBasic() {
+        log.info("helloBasic");
+        return "ok";
+    }
 }
