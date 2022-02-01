96c0681ff5a4f99aeffd9dd1dbac0051aa6ebce9
heroku test <wjstngus1031@gmail.com>
2022-02-01 23:44:41 +0900

    header 로그 호출

--- springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -3,7 +3,8 @@ package main.java.hello.springmvc.basic.requestmapping;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
-@RestController
+
+
 public class MappingController {
     private Logger log = LoggerFactory.getLogger(getClass());
 
@@ -12,4 +13,23 @@ public class MappingController {
         log.info("helloBasic");
         return "ok";
     }
+
+    @GetMapping(value = "/maaping-header", headers = "mode=debug")
+    public String mappingHeader(){
+        log.info("mappingHeader");
+        return  "ok";
+        //헤더에 mode는 디버그 이 값을 넣고 호출하면 저 헤더 조건에 맞는 경우 호출이 됨.
+    }
+
+    @PostMapping
+
+
+
+
+
+
+
+
+
+
 }
