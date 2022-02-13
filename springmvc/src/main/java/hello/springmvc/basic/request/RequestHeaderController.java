7ddf22ce5d1337cf739ad6dd2f868e1367dbb72b
heroku test <wjstngus1031@gmail.com>
2022-02-14 01:26:59 +0900

    http 요청 - 기본, 헤더 조회

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestHeaderController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestHeaderController.java
@@ -14,13 +14,11 @@ public class RequestHeaderController {
     @RequestMapping("/headers")
     public String headers(HttpServletRequest request,
                           HttpServletResponse response,
-                          HttpMethod httpMethod,
-                          Locale locale,
-                          @RequestHeader MultiValueMap<String, String>
-                                  headerMap,
-                          @RequestHeader("host") String host,
-                          @CookieValue(value = "myCookie", required = false)
-                                  String cookie
+                          HttpMethod httpMethod,//get put 같은거
+                          Locale locale,//언어정보 중 가장 우선순위 높은게 지정 됨
+                          @RequestHeader MultiValueMap<String, String> headerMap,//키도스트링 값도스트링
+                          @RequestHeader("host") String host,//헤더에 네임을 매핑
+                          @CookieValue(value = "myCookie", required = false) String cookie//쿠키값이 없어도 된다는 뜻
     ) {
         log.info("request={}", request);
         log.info("response={}", response);
@@ -31,4 +29,4 @@ public class RequestHeaderController {
         log.info("myCookie={}", cookie);
         return "ok";
     }
-}
+}//멀티 벨류 맵은 맵과 유사한데 하나의 키에 여러 값을 받을 수 있음. 같은 헤더에 여러 값 들어갈 경우 등. 하나의 키를 꺼내면 배열로 여러 값이 나옴
