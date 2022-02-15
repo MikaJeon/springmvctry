452ced17aa39ef5f73bf34fbf597a352d294906d
heroku test <wjstngus1031@gmail.com>
2022-02-16 02:45:55 +0900

    응답 - 정적 리소스, 뷰 템플릿

--- springmvc/src/main/java/hello/springmvc/basic/response/ResponseViewController.java
+++ springmvc/src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -10,14 +10,16 @@ public class ResponseViewController {
     public ModelAndView responseViewV1() {
         ModelAndView mav = new ModelAndView("response/hello")
                 .addObject("data", "hello!");
-        return mav;
+        return mav;//모델 앤 뷰 객체(리스폰스 밑의 헬로우라는 이름으로 있는)를 만들고, 데이타라는 변수안에 헬로우를 담아 추가. 그리고 리턴@
     }
-    @RequestMapping("/response-view-v2")
+    @RequestMapping("/response-view-v2")//리스폰스바디 쓰면 안된다! 뷰로 연결 안되고 문자로 찍혀버림
     public String responseViewV2(Model model) {
-        model.addAttribute("data", "hello!!");
+        model.addAttribute("data", "hello!!");//스트링으로 반환해서 뷰를 찾게 할거니까 model을 파라미터로 넣어줘야 함.
+        //모델이라는 객체에 데이터 라는 변수를 담아야 하니까!
         return "response/hello";
     }
-    @RequestMapping("/response/hello")
+    @RequestMapping("/response/hello")//경로의 이름과 뷰 이름이 같은 경우 리턴값이 생략 가능한 버전.. 추천하지 않음!
+    //@컨트롤러를 사용하고 http바디를 건드리는 파라미터가(httpresponse같은) 없으면 가능. 하지만 명시성이 없으니..비추천
     public void responseViewV3(Model model) {
         model.addAttribute("data", "hello!!");
     }
